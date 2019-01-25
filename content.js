d428c7a99b542495ac0a3f8bb5ecda021dc24f2c
Michael Provencher <michaelprovencher15@gmail.com>
2019-01-24 20:26:06 -0500

    fix layout wheb imdb logo is present

--- content.js
+++ content.js
@@ -51,6 +51,7 @@ function callApi() {
 function getRatings(movieData) {
   console.log(movieData)
   const ratings = movieData.Ratings;
+  const imdbAlreadyExistingSelector = document.querySelector('.imdb-rating');
   const rottenTomatoes = ratings.filter(rating => {
     return rating.Source == "Rotten Tomatoes";
   });
@@ -59,14 +60,15 @@ function getRatings(movieData) {
   });
 
   if (rottenTomatoes.length > 0) {
-    displayRTRating(rottenTomatoes);
+    displayRTRating(rottenTomatoes, imdbAlreadyExistingSelector);
   }
-  if (imdbRating.length > 0) {
+
+  if (imdbRating.length > 0 && imdbAlreadyExistingSelector === null) {
     displayIMDbRating(imdbRating);
   }
 }
 
-function displayRTRating(rottenTomatoesRating) {
+function displayRTRating(rottenTomatoesRating, differentLayout) {
   const rtRatingNode = document.createElement("DIV");
   const rtRatingClassNode = document.createElement("SPAN");
   const textnode = document.createTextNode(rottenTomatoesRating[0].Value);
@@ -74,17 +76,26 @@ function displayRTRating(rottenTomatoesRating) {
   const ratingNumber = rottenTomatoesRating[0].Value.slice(0, -1);
   
   console.log(ratingNumber)
-  if(ratingNumber < "60") {
+
+  if(parseInt(ratingNumber) < 60) {
 
     rottenStatus = 'rotten';
   }
 
-  rtRatingClassNode.classList.add('rating-other-source',`rt-${rottenStatus}-rating`)
+  rtRatingClassNode.classList.add('rating-other-source',`rt-${rottenStatus}-rating`);
+
+
 
   rtRatingNode.appendChild(rtRatingClassNode);
   rtRatingNode.appendChild(textnode);
 
-  appendNewRatings(rtRatingNode);
+  if(differentLayout) {
+    rtRatingNode.classList.add('rating-rt-with-imdb');
+    appendNewRatingsNewPlace(rtRatingNode);
+  } else {
+    appendNewRatings(rtRatingNode);
+  }
+
 }
 
 function displayIMDbRating(imdbRating) {
@@ -102,9 +113,17 @@ function displayIMDbRating(imdbRating) {
 
 function appendNewRatings(node) {
   const selectionExists = document.querySelector('#averageCustomerReviews') !== null;
+  
   if(selectionExists) {
     document.querySelector("#averageCustomerReviews").append(node);
   } else {
     document.querySelector("#bylineInfo_feature_div").append(node);
   }
+}
+
+function appendNewRatingsNewPlace(node) {
+  let seperator = document.createElement("I");
+  seperator.classList.add('a-icon','a-icon-text-separator');
+  node.prepend(seperator);
+  document.querySelector('#imdbInfo_feature_div').append(node);
 }
